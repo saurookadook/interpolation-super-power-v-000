9bc6c6cd3f4c72f3db9e9ef0c9932e115e2258da
Andy Maskiell <maskiella@gmail.com>
2017-10-20 15:39:59 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
 # Write your #display_rainbow method here
-def dispolay_rainbow(colors)
+def dispolay_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
   puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[7]}"
 end
